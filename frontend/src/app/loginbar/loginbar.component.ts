ad6b112d74cfaa99f3771ae0a3dede6b7c69ae8f
Giulia Gargiulo <giulia.gargiulo3@studenti.unina.it>
2026-05-15 18:05:31 +0200

    Some changes

--- frontend/src/app/loginbar/loginbar.component.ts
+++ frontend/src/app/loginbar/loginbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-loginbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './loginbar.component.html',
   styleUrl: './loginbar.component.scss'
 })
